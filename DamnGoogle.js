0ec0f0c5d9b357edd00289523bd0c9b4b415985f
adis24 <adi24s@gmail.com>
2018-10-03 21:54:17 -0700

    DamnGoogle

---
+++ DamnGoogle.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        Damn You Google!
+// @version     1
+// @author      Aditya Shrivastava
+// @description Too much security is bad
+// @include https://accounts.google.com/DisplayUnlockCaptcha
+// @include https://accounts.google.com/DisplayUnlockCaptcha/
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function damnGoogle() {
+
+        var cont = document.getElementById("submitChallenge");
+        if (cont === null) {
+            window.location.href = 'https://accounts.google.com/DisplayUnlockCaptcha';
+        } else {
+            cont.click();
+        }
+    }
+
+    setTimeout(damnGoogle, 10000);
+
+})();
